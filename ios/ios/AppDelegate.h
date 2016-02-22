28373e5bf2366e6c6a443418d175dd4f48afe173
zxh <feng3843@qq.com>
2016-02-22 11:52:14 +0800

    init

---
+++ ios/ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios
+//
+//  Created by ivy.sun on 16/2/22.
+//  Copyright © 2016年 zxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios
+//
+//  Created by ivy.sun on 16/2/22.
+//  Copyright © 2016年 zxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
